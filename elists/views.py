953129a973fe9745de3b5dbe3303f51528faf807
Illia Ananich <illia.ananich@gmail.com>
2018-07-11 21:38:48 +0300

    `json_error` - return error messages as JSON

--- elists/views.py
+++ elists/views.py
@@ -1,6 +1,6 @@
 import json
 
-from django.http import JsonResponse, HttpResponseBadRequest, HttpRequest
+from django.http import JsonResponse, HttpRequest
 from django.views.decorators.http import require_POST
 
 from .models import CheckInSession, Student, Staff
@@ -11,6 +11,12 @@ def get_staff():
     return Staff.objects.first()
 
 
+def json_error(message: str, code: int =400):
+    r = JsonResponse({'error': message})
+    r.status_code = code
+    return r
+
+
 @require_POST
 def start_new_session(request: HttpRequest):
     staff = get_staff()
@@ -18,7 +24,7 @@ def start_new_session(request: HttpRequest):
     if session:
         return JsonResponse({})
     else:
-        return HttpResponseBadRequest(b'Staff already has open session.')
+        return json_error('Staff already has open session.')
 
 
 @require_POST
@@ -26,17 +32,17 @@ def get_student_by_ticket_number(request: HttpRequest):
     staff = get_staff()
     session = CheckInSession.get_session_by_staff(staff)
     if session is None:
-        return HttpResponseBadRequest(b'No session assigned.')
+        return json_error('No session assigned.')
     ticket_number = json.loads(request.body)['ticket_number']
     try:
         student = Student.get_student_by_ticket_number(ticket_number)
     except (ValueError, IndexError) as exc:
-        return HttpResponseBadRequest(str(exc).encode('utf-8'))
+        return json_error(str(exc))
     session = CheckInSession.assign_student_to_session(session, student)
     if session:
         return JsonResponse({})
     else:
-        return HttpResponseBadRequest(b'Student not allowed to assign or session status is wrong.')
+        return json_error('Student not allowed to assign or session status is wrong.')
 
 
 @require_POST
@@ -44,13 +50,13 @@ def complete_session(request: HttpRequest):
     staff = get_staff()
     session = CheckInSession.get_session_by_staff(staff)
     if session is None:
-        return HttpResponseBadRequest(b'No session assigned.')
+        return json_error('No session assigned.')
     # FIXME: is_open check makes overhead
     session = CheckInSession.complete_session(session)
     if session:
         return JsonResponse({})
     else:
-        return HttpResponseBadRequest(b'Session was already closed.')
+        return json_error('Session was already closed.')
 
 
 @require_POST
@@ -58,10 +64,10 @@ def cancel_session(request: HttpRequest):
     staff = get_staff()
     session = CheckInSession.get_session_by_staff(staff)
     if session is None:
-        return HttpResponseBadRequest(b'No session assigned.')
+        return json_error('No session assigned.')
     # FIXME: is_open check makes overhead
     session = CheckInSession.cancel_session(session)
     if session:
         return JsonResponse({})
     else:
-        return HttpResponseBadRequest(b'Session was already closed.')
+        return json_error('Session was already closed.')
