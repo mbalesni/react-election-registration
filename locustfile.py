287e7e401afc949438a61d698649bd7129933134
Illia Ananich <illia.ananich@gmail.com>
2018-09-15 23:15:07 +0300

    `locustfile`: handle 1-1 user-student case

--- locustfile.py
+++ locustfile.py
@@ -1,7 +1,17 @@
+import os
+
 from locust import HttpLocust, TaskSet, task, clients
 
-TESTUSER_USERNAME = 'testuser'
-TESTUSER_PASSWORD = 'Testing0'
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "evs.settings")
+django.setup()
+from student.models import Student
+
+TEST_USER_USERNAME = 'testuser'
+TEST_USER_PASSWORD = 'Testing0'
+
+TEST_STUDENT_TICKET_NUMBER = '11111111'
 
 CSRF_TOKEN_COOKIE = 'csrftoken'
 SESSION_ID_COOKIE = 'sessionid'
@@ -17,6 +27,8 @@ class MyTaskSet(TaskSet):
     client: clients.HttpSession
 
     def on_start(self):
+
+        # prepare user
         admin_login_get_response = self.client.get(url=ADMIN_LOGIN_URL)
         csrf_token = admin_login_get_response.cookies.get(CSRF_TOKEN_COOKIE)
 
@@ -44,8 +56,89 @@ class MyTaskSet(TaskSet):
                 CSRF_TOKEN_COOKIE: self.csrf_token,
             },
         )
-        print(admin_logout_response.status_code)
+
+    # === --- ===
+
+    def api(self, path: str, data: dict =None) -> dict:
+        # if data is None:
+        #     json = b'{}'
+        # else:
+        #     json = None
+        if data is None:
+            json = {}
+
+        resp = self.client.post(
+            url=f'api/elists/{path}',
+            # data=data,
+            json=data,
+            cookies={
+                SESSION_ID_COOKIE: self.session_id,
+                CSRF_TOKEN_COOKIE: self.csrf_token,
+            },
+            # headers={
+            #     'Content-Type': 'application/json',
+            # }
+        )
+        return resp.json()
+
+    # === --- ===
+
+    @task(1)
+    def general_task(self):
+        # close_sessions
+        close_sessions_resp = self.api('close_sessions')
+        print(close_sessions_resp)
+
+        # me
+        me_resp = self.api('me')
+        print(me_resp)
+
+        # start_session
+        start_session_resp = self.api('start_new_session')
+        print(start_session_resp)
+        check_in_session_token = start_session_resp['data']['check_in_session']['token']
+
+        # search student
+        search_student_req = {
+            'check_in_session_token': check_in_session_token,
+            'student': {
+                'ticket_number': TEST_STUDENT_TICKET_NUMBER,
+            }
+        }
+        search_student_resp = self.api('search_by_ticket_number', search_student_req)
+        print(search_student_resp)
+        check_in_session_token = start_session_resp['data']['check_in_session']['token']
+
+        # submit student
+        submit_student_req = {
+            'check_in_session_token': check_in_session_token,
+            'student'               : {
+                'token': search_student_resp['data']['student']['token'],
+                'doc_type': 0,
+                'doc_num': TEST_STUDENT_TICKET_NUMBER,
+            }
+        }
+        submit_student_resp = self.api('submit_student', submit_student_req)
+        print(submit_student_resp)
+        check_in_session_token = start_session_resp['data']['check_in_session']['token']
+
+        # complete session
+        complete_session_req = {
+            'check_in_session_token': check_in_session_token,
+        }
+        complete_session_resp = self.api('complete_session', complete_session_req)
+        print(complete_session_resp)
+
+        # --- reset student
+        obj = Student.objects.get(
+            ticket_number=TEST_STUDENT_TICKET_NUMBER,
+        )
+        obj.status = Student.STATUS_FREE
+        obj.save()
 
 
 class MyLocust(HttpLocust):
     task_set = MyTaskSet
+
+    min_wait = 5 * 1000
+    max_wait = 5 * 1000
