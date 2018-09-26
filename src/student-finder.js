ba2802dc9a07a0f0501cfe8e2ce39f4668a43d94
Nick Baliesnyi <nikita.balesny@gmail.com>
2018-09-26 21:44:18 +0300

    improve name validation

--- src/student-finder.js
+++ src/student-finder.js
@@ -11,14 +11,29 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import TextField from '@material-ui/core/TextField';
 import Input from '@material-ui/core/Input';
 import './css/studentFinder.css'
+import { message } from 'antd'
+
+
+const MIN_LENGTH = {
+  name: 5,
+  docNumber: 3
+}
+const NAME_MIN_LENGTH = 5
+const DOCNUMBER_MIN_LENGTH = 3
 
 export default class StudentFinder extends React.Component {
   state = {
     docType: '0',
     isScanning: false,
     value: '0',
-    name: '',
-    docNumber: '',
+    name: {
+      value: '',
+      label: `ім'я`
+    },
+    docNumber: {
+      value: '',
+      label: `номер документа`
+    },
     touched: {
       name: false,
       docNumber: false,
@@ -45,15 +60,16 @@ export default class StudentFinder extends React.Component {
   handleSearch() {
     console.log('Handling search... ')
     let { name, value, docNumber } = this.state
-    this.props.onSearchByName(name, value, docNumber)
+    this.props.onSearchByName(name.value, value, docNumber.value)
 
   }
 
   validate(name, docNumber) {
-    // message string means invalid
+    // true condition means error
+    // string is error explanation
     return {
-      name: name.length < 3 && 'Ім\'я повинно бути 3 або більше символів в довжину' || '',
-      docNumber: docNumber.length < 3 && 'Номер документа повинен бути 3 або більше символів в довжину' || '',
+      name: (name.length < MIN_LENGTH.name || !isTitle(name) || !hasSpaces(name)) && `Ім'я повинно бути ${MIN_LENGTH.name} або більше символів в довжину` || '',
+      docNumber: docNumber.length < MIN_LENGTH.docNumber && `Номер документа повинен бути ${MIN_LENGTH.docNumber} або більше символів в довжину` || '',
     }
   }
 
@@ -66,8 +82,10 @@ export default class StudentFinder extends React.Component {
   handleSubmitOnEnter(e) {
     if (e.key === 'Enter') {
       const { name, docNumber } = this.state
-      const errors = this.validate(name, docNumber)
-      const noErrors = Object.keys(errors).some(x => errors[x].length === 0)
+      const errors = this.validate(name.value, docNumber.value)
+      const noErrors = !Object.keys(errors).some(x => errors[x].length > 0)
+      console.log('Errors: ', errors)
+      console.log('noErrors: ', noErrors)
 
       if (noErrors) this.handleSearch()
     } else return
@@ -77,14 +95,26 @@ export default class StudentFinder extends React.Component {
   render() {
     const { value, name, docNumber } = this.state
 
-    const errors = this.validate(name, docNumber)
+    const errors = this.validate(name.value, docNumber.value)
     const isDisabled = Object.keys(errors).some(x => errors[x].length > 0)
 
     const shouldMarkError = (field) => {
       const hasError = errors[field].length > 0
       const shouldShow = this.state.touched[field]
 
-      return hasError ? shouldShow : false
+
+      const shouldMarkErrorResult = hasError ? shouldShow : false
+      if (shouldMarkErrorResult) {
+        let text = ''
+        text += `Довжина повинна бути не меншою за ${MIN_LENGTH[field]} символів`
+        if (field === 'name') text += `, а ім'я повинно бути з великої літери`
+        // message.warn(text)
+      } else {
+        // message.destroy()
+      }
+
+
+      return shouldMarkErrorResult
     }
 
     return (
@@ -126,7 +156,7 @@ export default class StudentFinder extends React.Component {
                 className="input student-name"
                 error={shouldMarkError('name')}
                 placeholder="Повне ім'я"
-                value={this.state.name}
+                value={this.state.name.value}
                 fullWidth={true}
                 onChange={this.handleNameChange}
                 onBlur={this.handleBlur('name')}
@@ -138,7 +168,7 @@ export default class StudentFinder extends React.Component {
                 className="input doc-number"
                 error={shouldMarkError('docNumber')}
                 placeholder="Номер документа"
-                value={this.state.docNumber}
+                value={this.state.docNumber.value}
                 fullWidth={true}
                 onChange={this.handleDocNumberChange}
                 onBlur={this.handleBlur('docNumber')}
@@ -174,14 +204,35 @@ export default class StudentFinder extends React.Component {
   }
 
   handleNameChange = (e) => {
-    let name = e.target.value
+    let nameVal = e.target.value
+    let name = {...this.state.name}
+    name.value = nameVal
     this.setState({ name })
   }
 
   handleDocNumberChange = (e) => {
-    let docNumber = e.target.value
+    let docNumberVal = e.target.value
+    let docNumber = {...this.state.docNumber}
+    docNumber.value = docNumberVal
     this.setState({ docNumber })
   }
 
 
+}
+
+function toTitleCase(str) {
+  return str.replace(
+    /\S*/g,
+    function (txt) {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    }
+  )
+}
+
+function isTitle(str) {
+  return str === toTitleCase(str)
+}
+
+function hasSpaces(str) {
+  return /\s/g.test(str);
 }
