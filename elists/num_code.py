40989a775d4fc0b3f6fd4ecf4c2e16047815a431
Illia Ananich <illia.ananich@gmail.com>
2018-09-30 05:01:46 +0300

    rename `numcode.py`->`num_code.py`, add offset

---
+++ elists/num_code.py
@@ -0,0 +1,40 @@
+import logging
+
+from tgapp import tasks
+
+log = logging.getLogger('elists.numcode')
+
+
+class NumCodeGen:
+
+    def __init__(self):
+        self.__secret = 7_651_279_397_995
+        self.__offset = 37
+
+        self._result_space = 10**8
+        self._result_offset = 2 * 10**7
+
+        self._result_moduler = self._result_space - self._result_offset
+
+        self.log_secret(
+            secret=self.__secret,
+            offset=self.__offset,
+        )
+
+    @classmethod
+    def log_secret(cls, secret: int, offset: int):
+        msg = f'Numeric code generator initiated with secret=`{secret}` and offset=`{offset}`'
+        log.info(msg)
+
+        # TODO
+        # tasks.notify(msg)
+
+    def encode(self, data: int) -> int:
+        result = (data + self.__offset) * self.__secret
+        encoded = self._result_offset + result % self._result_moduler
+
+        log.debug(f'New numerical code generated for {data} - {encoded}')
+        return encoded
+
+
+num_code_generator = NumCodeGen()
