42fc035befa6ca6633591f6caeb49e887aa3f1f5
Nick Baliesnyi <nikita.balesny@gmail.com>
2018-10-01 19:36:33 +0300

    add year display back, disable student register button based on status

--- src/student-info.js
+++ src/student-info.js
@@ -20,42 +20,52 @@ export default class StudentInfo extends React.Component {
 
   componentDidMount() {
     if (this.props.activeStudent) this.setState({ submitted: true, disabled: true })
+    if (this.props.data.status !== 0) this.setState({ disabled: true })
   }
 
   render() {
-    const { name, degree, year, activeStudent, formOfStudy, specialty, structuralUnit } = this.props.data
+    const { name, degree, year, activeStudent, formOfStudy, specialty, structuralUnit, status } = this.props.data
     const student = this.props.data
     let { disabled, submitted } = this.state
     let classes = ['student']
     if (disabled) classes.push('disabled')
 
     let buttonName = ''
-    if (!submitted) buttonName = 'підтвердити'
-    if (submitted && this.props.activeStudent) buttonName = 'підтверджено'
-    if (submitted && !this.props.activeStudent) buttonName = 'відмовлено'
+
+    switch (status) {
+      case 0:
+        buttonName = 'зареєструвати'
+        break
+      case 1:
+        buttonName = 'в процесі'
+        break
+      case 2:
+        buttonName = 'вже зареєстровано'
+        break
+    }
 
     return (
-      <div className={classes.join(' ')}>
+        <div className={classes.join(' ')}>
 
-        <div className="data">
-          <div className="student--name">{name}</div>
-          <div className="student--info">
-            <div>{structuralUnit}</div>
-            <div>{specialty}</div>
-            <div>{degree} | {formOfStudy}</div>
+          <div className="data">
+            <div className="student--name">{name}</div>
+            <div className="student--info">
+              <div>{structuralUnit}</div>
+              <div>{specialty}</div>
+              <div>{year} курс | {degree} | {formOfStudy}</div>
+            </div>
           </div>
-        </div>
 
-        <div className="actions">
-          {!activeStudent &&
-            <Button disabled={disabled} variant="flat" color="primary" onClick={() => { this.handleSubmit(student) }}>
-              {buttonName}
-            </Button>
-          }
-        </div>
+          <div className="actions">
+            {!activeStudent &&
+              <Button disabled={disabled} variant="flat" color="primary" onClick={() => { this.handleSubmit(student) }}>
+                {buttonName}
+              </Button>
+            }
+          </div>
 
-      </div>
-    )
+        </div>
+      )
   }
 
 }
