e82e20aa36e337519812c0b9f4fee2b5317e47f5
Nick Baliesnyi <nikita.balesny@gmail.com>
2018-10-06 21:09:58 +0300

    add scanning window title

--- src/icons.js
+++ src/icons.js
@@ -1,9 +1,10 @@
 export const ICONS = {
+  camera: 'fas fa-camera',
   card: 'fas fa-id-card',
   readyIcon: 'fas fa-circle',
   scanningIcon: 'fas fa-video',
   searchingIcon: 'fas fa-search',
   scannedIcon: 'far fa-check-circle',
   errorIcon: 'fas fa-exclamation-circle',
-  userCheck: 'fas fa-user-check'
+  userCheck: 'fas fa-user-check',
 }
